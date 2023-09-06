87bf8ceefd7c08baae2278716c76ff57a5d4f32f
vanswei <1049651706@qq.com>
2023-09-06 16:21:01 +0800

    完善设备位置查询逻辑，优化提示词,将经纬度转换成城市地点。

--- server/agents.js
+++ server/agents.js
@@ -12,7 +12,7 @@ import { BufferMemory } from "langchain/memory"
 
 
 const prefix = "1.你是一个乐于助人的智能客服，但是每条输出的语句必须有礼貌的使用中文回答 2.查询设备状态时必须呈现设备的电量。"
-const suffix = "1.当输出的语句出现时间戳与相关时，必须转换成正常时间显示。2.设备状态的最晚登录时间早于最晚离线时间时判断为离线，否则判断为在线。3.输出的语句含有设备电量时，可以给出充电建议。"
+const suffix = "1.当输出的语句出现时间戳与相关时，必须转换成正常时间显示。2.设备状态的最晚登录时间早于最晚离线时间时判断为离线，否则判断为在线。3.输出的语句含有设备电量时，可以给出充电建议。4.输出语句中出现经度和维度时转换成大概的城市地点。"
 
 
 const model = new ChatOpenAI({
@@ -34,7 +34,7 @@ const tools = [
     }),
     func: async function (options) {
       const { sn } = options
-      const url = `http://localhost:8520/api/get-location?sn=${sn}`
+      const url = `http://localhost:8520/api/snlocation?sn=${sn}`
       const response = await fetch(url)
       const data = await response.json()
       const dataString = JSON.stringify(data)
